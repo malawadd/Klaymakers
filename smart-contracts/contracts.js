7786a179adeb254dcae99928bed17842102761da
malawad <50057882+malawadd@users.noreply.github.com>
2023-11-27 15:51:47 +0300

    💻 addresses

---
+++ smart-contracts/contracts.js
@@ -0,0 +1,16 @@
+
+    export const router = "0xAe467A4CfCe5310C50E2b2A1ad30768A02155fAC"
+    export const Trading = "0x24c23a634dC1dD033Dc2B2063bc689BD35BE610f"
+    export const oracle = "0xB11011307e0F3c805387c10aa69F874244b1bec3"
+    export const treasury = "0x804BCb3B87F93Ec42B672cda3f88A1978d6e884F"
+    export const AND = "0x16de95d9199Fceb3546565909eB52a4726B14311"
+    export const usdc = "0x3bBF3EaacC44b3ed11c1713D439aFC5f105871a5"
+    export const PoolAND = "0x58c7df5c9fD02EB77f617Ea4aE511612Dd85ce94"
+    export const poolETH = "0xa09861C51676CF0fB34b22F8e0199A507D8a26EA"
+    export const poolUSDC = "0xD0a999DB766c1a41Db113f725a851eb3ea6A2b3a"
+    export const poolRewardsETH = "0x1fC0d18d6045DCBD44933889496d5cf5F5360010"
+    export const poolRewardsUSDC = "0x7771117a5171B643C0aDaEfa863703ac929c2Ccf"
+    export const ANDRewardsETH = "0xA20aa0968C555cf984B52D530586108856a0A134"
+    export const ANDRewardsUSDC = "0x532C88424404a10A41A1a3bbD6016Aa0b27c909D"
+  
+    
